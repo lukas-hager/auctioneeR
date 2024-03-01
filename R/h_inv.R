d97357a29b24bbf56c9e2da04c002e48697a88dd
Lukas Hager <lghhager@uw.edu>
2024-03-01 10:36:30 -0800

    first function

--- R/h_inv.R
+++ R/h_inv.R
@@ -1,3 +1,17 @@
+#' Calculate the inverse transform of signals to posterior mean.
+#'
+#' Given the posterior mean is updated via \deqn{\mu_1 = h(\overline{x}) = \frac{k\mu_0 + n\overline{x}}{n+k}}
+#' We have that \deqn{\overline{x}_{n-2} = h^{-1}(\mu_1) = \frac{(n+k)\mu_1 - (n-2)\overline{x}_{n-2} - 2*x_i}{n-2}}
+#'
+#' @param mu_1 The posterior mean.
+#' @param mu_0 The prior mean.
+#' @param x_i The bidder's signal.
+#' @param n The number of bidders.
+#' @param k The coefficient on the signal standard variance.
+#'
+#' @return The sample mean required to generate this value of the posterior, given a bidder's signal.
+#' @export
+#'
 h_inv <- function(mu_1,mu_0,x_i,n,k){
   f <- (1*(n==2) + 2*(n>2))
 
---
+++ tests/testthat/test-h_inv.R
@@ -0,0 +1,11 @@
+test_that("gives the right constant", {
+  expect_equal(h_inv(2,2,2,10,2),2)
+})
+
+test_that("inverse works with more than two bidders", {
+  expect_equal((h_inv(5,4,3,10,2)*(10-2) + 2*3 + 2*4)/(2+10), 5)
+})
+
+test_that("inverse works with two bidders", {
+  expect_equal((h_inv(5,4,3,2,2) + 3 + 2*4)/(2+2), 5)
+})
